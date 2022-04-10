5e0d749a7d113167cad872235bcc1def3d691cd7
Roman Troian <heyekzo@gmail.com>
2022-04-09 19:49:49 -0600

    NavBarImprove

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+//implement NavBar menu highlight
+const section = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li a');
+
+window.addEventListener('scroll', function (e) {
+  let current = '';
+
+  section.forEach(function (section) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+
+    if (scrollY >= sectionTop - sectionHeight / 2) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLi.forEach(function (li) {
+    li.classList.remove('navigation__link-active');
+
+    if (li.classList.contains(current)) {
+      li.classList.add('navigation__link-active');
+    }
+  });
+});
+
 // Implement scrolling
 document
   .querySelector('.navigation__list')
